5233ef2a0def4acd3e03974c57f143e011c7b09f
dzw <dzwdzwdzwdzw@126.com>
2019-01-16 09:32:28 +0800

    新增核对商品页面（未完成）

---
+++ src/main/resources/static/checkout_pub/js/checkoutjs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dzw on 2019/1/15.
+ */
+$(function () {
+    $(".delivery-time-items div").click(function () {
+        $(this).attr("class","choose-delivery-selected").siblings("div").attr("class","");
+    })
+
+    $("add_addr")
+});
